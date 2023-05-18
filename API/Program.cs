1a03fcf9713411a1c224a419baf4223f9b830a02
mane936 <53373350+mane936@users.noreply.github.com>
2023-05-18 09:10:58 +0200

    setting up DB

--- API/Program.cs
+++ API/Program.cs
@@ -36,6 +36,7 @@ try
 {
   var context = services.GetRequiredService<DataContext>();
   context.Database.Migrate(); // Effectively using update command from commandline 
+  await Seed.SeedData(context);
 }
 catch (Exception ex)
 {
