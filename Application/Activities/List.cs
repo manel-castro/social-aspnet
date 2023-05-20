0ee3847498fdc2a4054cc216d3f7d13f3ae899cb
mane936 <53373350+mane936@users.noreply.github.com>
2023-05-20 11:54:55 +0200

    refactor and add delete route

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -24,6 +24,7 @@ namespace Application.Activities
 
       public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
       {
+
         return await _context.Activities.ToListAsync();
       }
     }
