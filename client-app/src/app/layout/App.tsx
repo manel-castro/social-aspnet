3dd04970bb115f73f3dd0c2c2e1350342bc197f2
mane936 <53373350+mane936@users.noreply.github.com>
2023-05-27 12:29:22 +0200

    refactor axios

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -4,6 +4,7 @@ import { Activity } from "../models/activity";
 import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import { v4 as uuid } from "uuid";
+import agent from "../api/agent";
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -13,13 +14,9 @@ function App() {
   const [editMode, setEditMode] = useState(false);
 
   useEffect(() => {
-    axios
-      .get<Activity[]>("http://localhost:5000/api/activities")
-      .then((res) => {
-        console.log("res; ", res.data);
-
-        setActivities(res.data);
-      });
+    agent.Activities.list().then((response) => {
+      setActivities(response);
+    });
   }, []);
 
   function handleSelectActivity(id: string) {
